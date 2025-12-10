73dbce1e98b05715f0ca9d55c83831e4857f4302
lukauu <156195421+lukauu@users.noreply.github.com>
2025-12-10 20:19:55 +0200

    Updated subscriber model

--- confetti_cuisine/models/subscriber.js
+++ confetti_cuisine/models/subscriber.js
@@ -23,6 +23,17 @@ const subscriberSchema = new Schema({
         required: true
     },
     vip: Boolean
+}, { timestamps: true });
+
+subscriberSchema.virtual("subscriberAge").get(function() {
+  const now = new Date();
+  const createdAt = this.createdAt;
+  const timeDifference = now - createdAt;
+  return Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 });
+
+subscriberSchema.methods.getInfo = function() {
+  return `Name: ${this.name} Email: ${this.email} Zip Code: ${this.zipCode} Vip: ${this.vip}`;
+};
 
 export const Subscriber = mongoose.model("Subscriber", subscriberSchema);
